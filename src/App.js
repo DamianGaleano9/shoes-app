e02b9dcff654d62dbe01ac98833ffd47b280cc48
Damian Galeano <dj.damian.b@hotmail.com>
2023-09-25 21:33:33 +0200

    Structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { NavBar } from "./components/nav-bar";
+import { Products } from "./components/products";
+import {Pages} from "./components/pages";
+
 
 function App() {
   return (
     <div className="App">
       <h1>Shoes App</h1>
+      <NavBar />
+      <Products/>
+      <Pages/>
     </div>
   );
 }
---
+++ src/components/nav-bar/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const NavBar = (props) => {
+    return (
+        <div>
+            <h1>NavBar</h1>
+        </div>
+    )
+}
