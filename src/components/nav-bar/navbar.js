5918839e4714a1da8e7df5d7ad9362da58fe4d74
Damian Galeano <dj.damian.b@hotmail.com>
2023-10-11 20:33:10 +0200

    Create base.scss

--- src/components/nav-bar/navbar.js
+++ src/components/nav-bar/navbar.js
@@ -2,21 +2,21 @@ import { NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRightToBracket, faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
-import logoJordan from '../images/logos/air-jordan-logo-png-transparent.png';
+import logoJordan from '../images/logos/logoMain.png';
 
 const NavBar = () => {
     return (
         <div className="navbar-wrapper">
             <div className="left-nav">
 
-                <div className="nav-link-wrapper">
+                <div className="nav-link-jordan">
                     <NavLink to="/" >
                         <img src={logoJordan} />
                     </NavLink>
 
                 </div>
 
-                <div className="nav-link-wrapper">
+                <div className="nav-link-products">
                     <NavLink to="/products">
                         products
                     </NavLink>
